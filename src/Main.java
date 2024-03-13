7b56533a0d72e9c16c54cc911c8e8ddb67e76bd4
chris <drikosx323@gmail.com>
2024-03-13 14:55:55 +0200

    Implemented generateBinaryNumbers using Queue

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.util.LinkedList;
 import java.util.NoSuchElementException;
+import java.util.Queue;
 
 public class Main {
 
@@ -43,6 +45,20 @@ public class Main {
             rear = null;
         }
         length--;
+        return result;
+    }
+
+    public String[] generateBinaryNumbers(int n){
+        String[] result = new String[n];
+        Queue<String> q = new LinkedList<>();
+        q.offer("1");
+        for (int i=0; i<n; i++){
+            result[i] = q.poll();
+            String n1 = result[i] +"0";
+            String n2 = result[i] + "1";
+            q.offer(n1);
+            q.offer(n2);
+        }
         return result;
     }
 
