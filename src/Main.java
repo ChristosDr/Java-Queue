6bb527a93786d02fc40119b1a59c3fc72cd981cc
chris <drikosx323@gmail.com>
2024-03-13 14:20:05 +0200

    Implemented enqueue, dequeue and print method

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 public class Main {
 
     private ListNode front;
@@ -19,8 +21,52 @@ public class Main {
     public boolean isEmpty(){
         return length ==0;
     }
+
+    public void enqueue(int data){
+        ListNode temp = new ListNode(data);
+        if (isEmpty()){
+            front = temp;
+        }else {
+            rear.next = temp;
+        }
+        rear = temp;
+        length++;
+    }
+
+    public int dequeue(){
+        if (isEmpty()){
+            throw new NoSuchElementException();
+        }
+        int result = front.data;
+        front = front.next;
+        if (front == null){
+            rear = null;
+        }
+        length--;
+        return result;
+    }
+
+    public void print(){
+        if (isEmpty()){
+            return;
+        }
+
+        ListNode current = front;
+        while (current != null){
+            System.out.print(current.data+"-->");
+            current = current.next;
+        }
+        System.out.print("null");
+    }
+
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Main sll = new Main();
+        sll.enqueue(10);
+        sll.enqueue(15);
+        sll.enqueue(20);
+
+        sll.dequeue();
+        sll.print();
     }
 }
